7f85d0db331f6506d08990dfb67fa300b72702d8
phsiao7 <94072513+hsiaopat@users.noreply.github.com>
2023-01-12 18:47:37 -0600

    fixed modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { VideoStorageModule } from './video-storage/video-storage.module';
-import { QuestionStorageModule } from './question-storage/question-storage.module';
+import { VideoStorageModule } from './videos/videos.module';
+import { QuestionStorageModule } from './questions/questions.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
