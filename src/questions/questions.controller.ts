0dddd8a13087c38c52a9147c8a3353c16263d6f2
phsiao7 <94072513+hsiaopat@users.noreply.github.com>
2023-02-11 10:17:37 -0500

    Added components for post question. Need to do DTO

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,15 +1,20 @@
 /* This is a preliminary service. WILL CHANGE IN THE FUTURE ONCE WE FLESH OUT ER DIAGRAMS */
 
+/* HAVE TO MAKE QUESTION DTO OBJECT */
+
 import { Controller, Delete, Get, Post } from '@nestjs/common';
 import { Question } from './questions.entity';
 import { QuestionsService } from './questions.service';
 
 @Controller()
-export class UsersController {
+export class QuestionsController {
     constructor(private readonly questionsService: QuestionsService){}
-
+    
     @Post()
-    createQuestion(){}
+    async createQuestion(@Body() createQuestionDto: CreateQuestionDto) {
+        return this.questionsService.createQuestion(createQuestionDto);
+    }
+
 
     @Get()
     findAll(): Promise<Question[]> {
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,22 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { InsertValuesMissingError, Repository } from 'typeorm';
-import { User } from './questions.entity';
+import { Question } from './questions.entity';
 
 @Injectable()
 export class QuestionsService {
   constructor(
     @InjectRepository(Question)
-    private usersRepository: Repository<User>,
+    private usersRepository: Repository<Question>,
   ) {}
 
-  createUser(){}
+  async createQuestion(createQuestionDto: CreateQuestionDto): Promise<Question> {
+    // implementation to create a new question using the data from createQuestionDto
+    const newQuestion = {
+      // example properties, replace with actual properties as needed
+      id: Date.now(),
+      Company: createQuestionDto.Company,
+      Question: createQuestionDto.Question,
+      Role: createQuestionDto.Role,
+      isActive: createQuestionDto.isActive,
+    };
 
-  findAll(): Promise<User[]> {
+    // Example: saving the new question to a database
+    this.questions.push(newQuestion);
+
+    return newQuestion;
+  }
+
+  findAll(): Promise<Question[]> {
     return this.usersRepository.find();
   }
 
-  findOne(id: number): Promise<User> {
+  findOne(id: number): Promise<Question> {
     return this.usersRepository.findOneBy({ id });
   }
 
