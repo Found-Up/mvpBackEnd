77c3216b6abbc5cbecd68cd9c399e9e484b38e4c
MattAlvero <matthewadrianoalvero@gmail.com>
2023-02-20 19:51:24 -0600

    made base repository interface and abstract class

---
+++ src/repositories/base/base.abstract.repository.ts
@@ -0,0 +1,44 @@
+import { BaseInterfaceRepository } from './base.interface.repository';
+import {
+  DeleteResult,
+  FindOneOptions,
+  FindOptionsWhere,
+  Repository,
+  UpdateResult,
+} from 'typeorm';
+import { QueryPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+
+export abstract class BaseAbstractRepository<T>
+  implements BaseInterfaceRepository<T>
+{
+  private entity: Repository<T>;
+
+  protected constructor(entity: Repository<T>) {
+    this.entity = entity;
+  }
+
+  public async create(data: any): Promise<T> {
+    return await this.entity.save(data);
+  }
+
+  public async getById(id: FindOneOptions<T>): Promise<T> {
+    return await this.entity.findOne(id);
+  }
+  public async getAll(): Promise<T[]> {
+    return await this.entity.find();
+  }
+  public async getByCondition(
+    filterCondition: FindOptionsWhere<T>,
+  ): Promise<T[]> {
+    return await this.entity.findBy(filterCondition);
+  }
+  public async update(
+    updateConditions: any,
+    data: QueryPartialEntity<T> | any,
+  ): Promise<UpdateResult> {
+    return await this.entity.update(updateConditions, data);
+  }
+  public async delete(id: string): Promise<DeleteResult> {
+    return await this.entity.delete(id);
+  }
+}
